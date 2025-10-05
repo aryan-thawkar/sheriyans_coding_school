594bffbfb04aa2e8221bdadb3abb91aa28e10a83
aryan <aryanthawakar975@gmail.com>
2025-10-05 23:47:23 +0530

    day 34 , V pattern and X pattern

--- DSA/34 nested loops/tutorial/index.js
+++ DSA/34 nested loops/tutorial/index.js
@@ -25,3 +25,29 @@ for(i = 1 ; i <= n ; i++){
     console.log();
 }
 
+// v shape
+
+for(i = 1 ; i <= n ; i++){
+        for (j = 1 ; j <= n*2  ; j++){
+            if (i == j || i+j == n*2 ){
+                process.stdout.write("*");
+            }
+            else{
+                process.stdout.write(" ");
+            }
+        }
+        console.log(" ");
+    }
+
+
+
+// X pattern 
+for (let i = 1; i <= n; i++) { 
+        for (let j = 1; j < n+1; j++) { 
+            if(i == j || i+j == n+1)
+               process.stdout.write("*"); 
+            else
+              process.stdout.write(" "); 
+        }
+        console.log();
+    }

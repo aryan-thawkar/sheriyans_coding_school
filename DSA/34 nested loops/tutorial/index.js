b54f0c598191447967606804077dc7b569302faf
aryan <aryanthawakar975@gmail.com>
2025-10-05 23:21:30 +0530

    mirrored triangle and complete triangle

--- DSA/34 nested loops/tutorial/index.js
+++ DSA/34 nested loops/tutorial/index.js
@@ -1,10 +1,23 @@
-// right aligned triangle
+// mirrored right aligned triangle
 
 const prompt = require('prompt-sync')();
 var n = prompt("enter a number : ");
 for(i = 1 ; i <= n ; i++){
     for(j = 1 ; j <= n-i ; j++){
         process.stdout.write("  ");
+    }
+    for(k = 1 ; k<= i; k++){
+        process.stdout.write(" *");
+    }
+    console.log();
+}
+
+
+
+// triangle pattern 
+for(i = 1 ; i <= n ; i++){
+    for(j = 1 ; j <= n-i ; j++){
+        process.stdout.write(" ");
     }
     for(k = 1 ; k<= i; k++){
         process.stdout.write(" *");

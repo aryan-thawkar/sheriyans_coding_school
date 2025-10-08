fda586b554ab53bc2f29078dd88dfc4b63d76a7a
aryan <aryanthawakar975@gmail.com>
2025-10-08 22:42:22 +0530

    day 36 second max no. in array

---
+++ DSA/36 more on arrays/tutorials/script.js
@@ -0,0 +1,22 @@
+// second max no in array 
+
+let arr = [28 , 6,74,48,84,79];
+if (arr[0] > arr[1]){
+    max = arr[0];
+    sMax = arr[1]
+}
+else{
+    max = arr[1];
+    sMax = arr[0]
+}
+
+for(let i = 2 ; i < arr.length ; i++){
+    if(arr[i] > max ){
+        sMax = max;
+        max = arr[i];
+    }else if (arr[i] > sMax){
+        sMax = arr[i];
+    }
+}
+
+console.log(sMax)

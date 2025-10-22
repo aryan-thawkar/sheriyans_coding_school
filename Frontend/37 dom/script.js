88d5628643a9d1bb794b864ac8dccd44814a8508
aryan <aryanthawakar975@gmail.com>
2025-10-23 01:12:23 +0530

    day 37 basic DOM introduction completed

---
+++ Frontend/37 dom/script.js
@@ -0,0 +1,8 @@
+var h = document.querySelector('h1');
+// console.log(h);
+
+h.addEventListener('click' , function(){
+     h.innerHTML = 'clicked on hello';
+      h.style.color = 'red';
+    console.log("hey guys");
+ })

cf326086e28e4f759748f1a493b06fcde5fbb247
aryan <aryanthawakar975@gmail.com>
2026-01-10 19:57:20 +0530

    44 : DOM getAttribute and setAttribute ( swap img mini program)

--- DSA/43 Mastering Multi Dimensional Array/index.js
+++ DSA/43 Mastering Multi Dimensional Array/index.js
@@ -1,13 +1,19 @@
 const prompt = require("prompt-sync")()
-let size = Number(prompt("Enter outer array length : "));
-let innerSize = Number(prompt("Enter inner array length : "));
-let arr = new Array(size);
 
-for(i = 0 ; i< size ; i++){
-    arr[i] = new Array(innerSize)
-    for(j = 0 ; j < innerSize ; j++){
-        arr[i][j] = Number(prompt(`enter value for ${i} ${j} : `));
-    }
-}
+let i = 0
+let arr1 = Array.from({length : 3}, ()=> Array(4).fill(i++));
+console.log(arr1);
 
-console.log(arr);
+
+// let size = Number(prompt("Enter outer array length : "));
+// let innerSize = Number(prompt("Enter inner array length : "));
+// let arr = new Array(size);
+
+// for(i = 0 ; i< size ; i++){
+//     arr[i] = new Array(innerSize)
+//     for(j = 0 ; j < innerSize ; j++){
+//         arr[i][j] = Number(prompt(`enter value for ${i} ${j} : `));
+//     }
+// }
+
+// console.log(arr);
